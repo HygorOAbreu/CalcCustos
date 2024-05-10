055ff3319005b1cf5533bc5bcd47be32208f2dff
Hygor O Abreu <HygorOAbreu@gmail.com>
2024-05-10 15:46:58 -0300

    att

--- Models/TratamentosDeErros.cs
+++ Models/TratamentosDeErros.cs
@@ -11,14 +11,25 @@ namespace CalcCustos.Models
         // valida se a informação inserida é um valor decimal.
         public decimal ObterValorNumeroTela()
         {
+
             while (true)
             {
                 // validação de "." e "," aceitando qualquer um dos caracteres para a execução do código de forma correta.
-                // Validação de valor inferior a 0.
-                // Caso as validações falhem, o usuario receberá uma notificação "generica" e uma nova chance de inserir a informação correta.
-                if (decimal.TryParse(Console.ReadLine().Replace('.', ','), out decimal result) && result > 0) return result;
-                Console.WriteLine("O valor precisa ser um número maior que 0");
-
+                if (decimal.TryParse(Console.ReadLine().Replace('.', ','), out decimal result))
+                    {
+                    if (result > 0)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        // validação se o valor digitado é um número.
+                        Console.WriteLine("O valor precisa ser um número maior que 0");
+                    }
+                } else {
+                    // Validação de valor é inferior a 0.                
+                    Console.WriteLine("O valor precisa ser um número");
+                }
             }           
             
         }
